30cd52d797024feeb6685cda33abcae276e84610
Sequin13 <116645119+Sequin13@users.noreply.github.com>
2024-01-09 00:00:29 +0100

    Added animated map for airplane route

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 from itertools import permutations
+from matplotlib.animation import FuncAnimation
 
 airports = {
     "Katowice": (50.4745, 19.0808),
@@ -10,16 +14,15 @@ airports = {
     "Ankara": (40.1289, 32.9959)
 }
 
+
 def cities_connection(cities):
-    list_of_connections=[]
+    list_of_connections = []
     for city in cities.keys():
         for city2 in cities.keys():
             if city < city2:
                 list_of_connections.append((city, cities[city], city2, cities[city2]))
-    # print(list_of_connections)
     return list_of_connections
 
-cities_connection(airports)
 
 def haversine(list_of_connections):
     dic = {}
@@ -31,18 +34,19 @@ def haversine(list_of_connections):
 
         con = list_of_connections[i][0] + " - " + list_of_connections[i][2]
 
-        dLat = (lat2 - lat1) * math.pi / 180.0
-        dLon = (lon2 - lon1) * math.pi / 180.0
-        lat1 = (lat1) * math.pi / 180.0
-        lat2 = (lat2) * math.pi / 180.0
+        dLat = (lat2 - lat1) * np.pi / 180.0
+        dLon = (lon2 - lon1) * np.pi / 180.0
+        lat1 = (lat1) * np.pi / 180.0
+        lat2 = (lat2) * np.pi / 180.0
 
-        a = (pow(math.sin(dLat / 2), 2) +
-             pow(math.sin(dLon / 2), 2) *
-             math.cos(lat1) * math.cos(lat2))
+        a = (np.sin(dLat / 2) ** 2 +
+             np.sin(dLon / 2) ** 2 *
+             np.cos(lat1) * np.cos(lat2))
         rad = 6371
-        c = 2 * math.asin(math.sqrt(a))
+        c = 2 * np.arcsin(np.sqrt(a))
         dic[con] = round(rad * c)
     return dic
+
 
 def calculate_total_distance(route, distances):
     total_distance = 0
@@ -60,6 +64,7 @@ def calculate_total_distance(route, distances):
 
     return total_distance
 
+
 def tsp_bruteforce(distances):
     cities = set()
     for key in distances:
@@ -75,16 +80,46 @@ def tsp_bruteforce(distances):
         if total_distance < shortest_distance:
             shortest_distance = total_distance
             shortest_route = route_permutation
-
     return shortest_route, shortest_distance
 
 
+def draw_shortest_route(frame):
+    plt.clf()
+    m = Basemap(projection='mill', llcrnrlat=30, urcrnrlat=75,
+                llcrnrlon=-30, urcrnrlon=60, resolution='c')
+    m.drawcoastlines()
+    m.drawcountries()
+
+    route_coordinates = [airports[city] for city in shortest_route[:frame+1]]
+    route_coordinates = np.array(route_coordinates)
+
+    x, y = m(route_coordinates[:, 1], route_coordinates[:, 0])
+    m.plot(x, y, 'mo--', markersize=10, linewidth=2)
+
+    for city, coordinates in airports.items():
+        x, y = m(coordinates[1], coordinates[0])
+        m.plot(x, y, 'ro', markersize=8)
+        plt.text(x, y, city, fontsize=12, ha='left', color = 'blue')
+
+    plt.title(f'Optimal route for DreamFlight airline')
+    plt.axis('off')
+    plt.subplots_adjust(bottom=0.1)
+    shortest_route_str=""
+    for airport in shortest_route:
+        shortest_route_str+=airport+" - "
+    shortest_route_str = shortest_route_str[:-2]
+    plt.text(0.5, -0.05, f'Shortest route: {shortest_route_str}', fontsize=12, ha='center', transform=ax.transAxes)
+    plt.text(0.5, -0.09, f'Shortest distance: {shortest_distance} km', fontsize=12, ha='center', transform=ax.transAxes)
+
 
 distances = haversine(cities_connection(airports))
-# for i in distances.items():
-#     print(i)
-
 shortest_route, shortest_distance = tsp_bruteforce(distances)
+
 print("Najkrótsza trasa:", shortest_route)
 print("Najkrótsza odległość:", shortest_distance)
+
+fig, ax = plt.subplots(figsize=(12, 10))
+ani = FuncAnimation(fig, draw_shortest_route, frames=len(shortest_route), interval=200, repeat=True)
+
+plt.show()
 
